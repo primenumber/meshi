a8c2827df60ceabbed9a8a2d16d6d26d416db58f
prime number <primenumber.proj@gmail.com>
2014-07-07 04:02:39 +0900

    Create meshi.rb

---
+++ meshi.rb
@@ -0,0 +1,22 @@
+#-*- coding: utf-8
+require 'uri'
+require 'open-uri'
+require 'json'
+
+Plugin.create(:meshi) do
+  filter_gui_postbox_post do |gui_postbox|
+    buf = Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer
+    text = buf.text
+    if /<<([[:word:]]+)>>/ =~ text then
+      puts $1
+      uri = URI.escape("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+$1)
+      response=open(uri)
+      data = JSON.load(response.read)
+      str = data["responseData"]["results"][0]["url"]
+      puts str
+      text = str
+    end
+    buf.text = text
+    [gui_postbox]
+  end
+end
